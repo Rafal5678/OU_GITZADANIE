14c2cbd050579895494690c34834e89cabdc3880
Rafal5678 <240767@edu.p.lodz.pl>
2022-01-20 15:05:42 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_GITZADANIE.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_GITZADANIE.dir/main.cpp.obj"
+  "OU_GITZADANIE.exe"
+  "OU_GITZADANIE.exe.manifest"
+  "OU_GITZADANIE.pdb"
+  "libOU_GITZADANIE.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_GITZADANIE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OU_GITZADANIE.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OU_GITZADANIE.
+# This may be replaced when dependencies are built.
